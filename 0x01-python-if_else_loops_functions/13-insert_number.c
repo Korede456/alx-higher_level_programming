5cce5923aa3b0003c338af11cde966a57252e070
korede thompson <koredeodeyemi456@gmail.com>
2024-01-03 22:28:02 +0000

    Singliy linked list i C

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,56 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * insert_node - returns a new sorted linked list
+ * @head: pointer to a linked list
+ * @number: a node data
+ *
+ * Return: address of a new node
+ */
+
+listint_t *insert_node(listint_t **head, int number);
+
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *newNode;
+	listint_t *current;
+
+	/* allocate memory for the new node */
+
+	newNode = malloc(sizeof(listint_t));
+
+	/* return Null if memory allocation fails */
+
+	if (!newNode)
+	{
+		return (NULL);
+	}
+	/* initialize newNode */
+
+	newNode->n = number;
+	newNode->next = NULL;
+
+	if (*head == NULL || number < (*head)->n)
+	{
+		newNode->next = *head;
+		*head = newNode;
+	}
+
+	/* initialize current */
+	current = *head;
+
+/* look for the right place to place the newNode if */
+/* the above condition was never met*/
+
+	while (current->next != NULL && number > current->next->n)
+	{
+		current = current->next;
+	}
+
+	newNode->next = current->next;
+	current->next = newNode;
+
+	return (newNode);
+}
