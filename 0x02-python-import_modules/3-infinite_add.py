ef96ba6b1bbe373308dabcf800dc45201aa141ab
korede thompson <koredeodeyemi456@gmail.com>
2024-01-04 21:43:21 +0000

    Python Script

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+
+    argsum = 0
+
+    if len(sys.argv) == 1:
+         print("{}".format(argsum))
+    else:
+        for i in range(1, len(sys.argv)):
+            argsum += (int(sys.argv[i]))
+        print("{}".format(argsum))
