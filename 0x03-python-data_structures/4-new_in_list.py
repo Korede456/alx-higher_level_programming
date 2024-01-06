6737f988f88bb160e2c7df1fe29c2e686091648c
korede thompson <koredeodeyemi456@gmail.com>
2024-01-06 09:13:23 +0000

    Python Script

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -4,12 +4,9 @@ def new_in_list(my_list, idx, element):
 
 
 def new_in_list(my_list, idx, element):
-    if idx < 0:
+    if idx < 0 or idx >= len(my_list):
         return my_list
-    elif idx > (len(my_list) - 1):
-        return my_list
-    else:
-        new_list = []
-        for item in my_list:
-            new_list.append(item)
-        return new_list
+    new_list = []
+    for item in my_list:
+        new_list.append(item)
+    return new_list
