d76da3db738716e8f6b0434ea8480313e059f76c
korede thompson <koredeodeyemi456@gmail.com>
2024-01-07 10:35:05 +0000

    Python Script

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    pass
+
+
+def print_matrix_integer(matrix=[[]]):
+    for i in matrix:
+        for j in i:
+            print("{}".format(j), end=" ")
+        print()
