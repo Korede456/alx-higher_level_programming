abd43f17b6df45a6611306c740323c572904a1f6
korede thompson <koredeodeyemi456@gmail.com>
2024-01-07 22:00:51 +0000

    Python Script

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -6,8 +6,5 @@ def delete_at(my_list=[], idx=0):
 def delete_at(my_list=[], idx=0):
     if idx < 0 or idx > len(my_list) - 1:
         return my_list
-    new_list = []
-    for item in my_list:
-        if item not in [my_list[idx]]:
-            new_list.append(item)
+    new_list = my_list[:idx] + my_list[idx + 1:]
     return new_list
