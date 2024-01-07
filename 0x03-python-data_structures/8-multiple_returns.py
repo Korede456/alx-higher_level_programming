51953d49be23bf55f17e955efae933ea49dd99e6
korede thompson <koredeodeyemi456@gmail.com>
2024-01-07 18:25:24 +0000

    Python Script

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    pass
+
+
+def multiple_returns(sentence):
+    length = len(sentence)
+    first = sentence[0] or None
+    return (length, first)
