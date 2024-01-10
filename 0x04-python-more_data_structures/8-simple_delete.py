c3a04282d97bd18e7cdbddaad8b35575d7dc80d8
korede thompson <koredeodeyemi456@gmail.com>
2024-01-10 14:59:00 +0000

    Delete by Key: detetes a key in a dictionary

---
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def simple_delete(a_dictionary, key=""):
+    pass
+
+
+def simple_delete(a_dictionary, key=""):
+    new_dictionary = a_dictionary
+
+    del((new_dictionary)[key])
+    return new_dictionary
