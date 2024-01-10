1ee44f088e861117737a10bf627ac3f48708af62
korede thompson <koredeodeyemi456@gmail.com>
2024-01-10 12:07:58 +0000

    find differences: return a set of all elements present in only one of two sets

---
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def only_diff_elements(set_1, set_2):
+    pass
+
+
+def only_diff_elements(set_1, set_2):
+    new_set = []
+    def func(set_1={}, set_2{}):
+        for item in set_1:
+            if item not in set_2:
+                new_set.append(item)
+    func(set_1, set_2)
+    func(set_2, set_1)
+
+    return new_set
