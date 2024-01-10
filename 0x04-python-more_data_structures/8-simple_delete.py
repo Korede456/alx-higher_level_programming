800b9a2a2b9f4b59244b793b0f9265f50d6f3550
korede thompson <koredeodeyemi456@gmail.com>
2024-01-10 16:02:54 +0000

    Delete keys: Deletes a key in a dictionary :

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -4,10 +4,6 @@ def simple_delete(a_dictionary, key=""):
 
 
 def simple_delete(a_dictionary, key=""):
-    new_dict = {}
-
-    for prop, value in a_dictionary.items():
-        if prop != key:
-            new_dict[prop] = value
-
-    return new_dict
+    if key in a_dictionary:
+        del a_dictionary[key]
+    return a_dictionary
