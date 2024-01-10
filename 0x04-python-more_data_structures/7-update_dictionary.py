02b5f7cecc821302985b490f599850219d769741
korede thompson <koredeodeyemi456@gmail.com>
2024-01-10 13:12:48 +0000

    Dictionary Update

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def update_dictionary(a_dictionary, key, value):
+    pass
+
+
+def update_dictionary(a_dictionary, key, value):
+    new_dictionary = a_dictionary
+    (new_dictionary)[key] = value
+    print(new_dictionary)
