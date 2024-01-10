d56d415a69305dd864f531728b422778de5c084d
korede thompson <koredeodeyemi456@gmail.com>
2024-01-10 12:56:22 +0000

    Sorted Dictionary: prints a dictionary by ordered keys

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -4,4 +4,6 @@ def print_sorted_dictionary(a_dictionary):
 
 
 def print_sorted_dictionary(a_dictionary):
-    print(dict(sorted(a_dictionary)))
+    sorted_key = sorted(a_dictionary)
+    for key in sorted_key:
+        print("{}: {}".format(key, (a_dictionary)[key]))
