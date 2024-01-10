779a16c92a774b359312888a2141407f1f7668af
korede thompson <koredeodeyemi456@gmail.com>
2024-01-10 11:12:56 +0000

    Unique Addition: adds all unique integers in a list

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    pass
+
+
+def uniq_ass(my_list=[]):
+    new_list = []
+    uniq_sum = 0
+    for item in my_list:
+        if item not in new_list:
+            new_list.append(item)
+            uniq_sum += item
+    return uniq_sum
