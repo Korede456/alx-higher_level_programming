a63f22bcea7a7cfd89e5a27c15220d663d5285ab
korede thompson <koredeodeyemi456@gmail.com>
2024-01-10 16:18:51 +0000

    Multiply all values in a dictionary

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -6,7 +6,7 @@ def multiply_by_2(a_dictionary):
 def multiply_by_2(a_dictionary):
     new_dict = {}
 
-    for key, value in a_dictionary.item():
+    for key, value in a_dictionary.items():
         new_dict[key] = value * 2
 
     return new_dict
