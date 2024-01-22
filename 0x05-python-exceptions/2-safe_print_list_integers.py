434a3468fc1a125adedc8c57d14b49e85c7c538f
korede thompson <koredeodeyemi456@gmail.com>
2024-01-22 19:48:07 +0000

    Python Exceptions

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+def safe_print_list_integers(my_list=[], x=0):
+    count = 0
+    idx = 0
+
+    try:
+        while idx < x:
+            value = my_list[idx]
+            if type(value) == int:
+                print("{:d}".format(my_list[idx]), end="")
+                count += 1
+            idx += 1
+
+    except IndexError:
+        pass
+    finally:
+        print()
+    return count
