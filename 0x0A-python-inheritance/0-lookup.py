4e013139ce1b7c7dd8ad6e2d6e3ad25665fd4a8f
korede thompson <koredeodeyemi456@gmail.com>
2024-02-05 21:03:45 +0000

    Python Inheritance

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -4,9 +4,4 @@
 
 def lookup(obj):
     """Return a list of an object's available attributes."""
-
-    list = []
-    for attr in dir(obj):
-        if not callable(getattr(obj, attr)) or callable(getattr(obj, attr)):
-            list.append(attr)
-    return (list)
+    return (dir(obj))
