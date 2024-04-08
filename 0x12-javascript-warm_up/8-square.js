ea117b1f5e0db58554ff15f94c967ac5606e7cb1
Odeyemi Korede <koredethompson456@gmail.com>
2024-04-08 07:01:49 -0700

    JavaScript

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -2,19 +2,19 @@
 
 const args = process.argv.slice(2);
 
-if (args.length > 0){
-	const firstArg = parseInt(args[0]);
-	if (!isNaN(firstArg)){
-		for (let i = 0; i < firstArg; i++){
-			let output = ''
-			for (let j = 0; j < firstArg; j++){
-				output.concat('X');
-			}
-			console.log(output);
-		}
-	} else {
-		console.log('Missing size');
-	}
+if (args.length > 0) {
+  const firstArg = parseInt(args[0]);
+  if (!isNaN(firstArg)) {
+    for (let i = 0; i < firstArg; i++) {
+      let output = '';
+      for (let j = 0; j < firstArg; j++) {
+        output = output.concat('X');
+      }
+      console.log(output);
+    }
+  } else {
+    console.log('Missing size');
+  }
 } else {
-	console.log('Missing size');
+  console.log('Missing size');
 }
