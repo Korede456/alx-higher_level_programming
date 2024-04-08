31a08f8637b308c972af3449796fcba016006ee0
Odeyemi Korede <koredethompson456@gmail.com>
2024-04-08 11:12:54 -0700

    JavaScript

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
 
-if (process.argv.length <= 3){
-	console.log(0);
-}
-else {
-	let args = process.argv.slice(2);
-	args.sort((a, b) => a - b);
-	console.log(args[args.length - 2]);
+if (process.argv.length <= 3) {
+  console.log(0);
+} else {
+  const args = process.argv.slice(2);
+  args.sort((a, b) => a - b);
+  console.log(args[args.length - 2]);
 }
