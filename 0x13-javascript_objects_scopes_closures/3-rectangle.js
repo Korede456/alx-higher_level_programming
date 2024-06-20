5cc473a0a2952042a16a4fc44b1576da166bfe6b
Korede Odeyemi <koredethompson456@gmail.com>
2024-06-20 10:59:46 -0700

    JavaScript

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -6,15 +6,16 @@ module.exports = class Rectangle {
     if ((w > 0) && (h > 0)) {
       this.width = w;
       this.height = h;
-      this.print = function () {
-        for (let i = 0; i < this.height; i++) {
-          let shape = '';
-          for (let j = 0; j < this.width; j++) {
-            shape += 'X';
-          }
-          console.log(shape);
-        }
-      };
     }
   }
+
+  print = function () {
+    for (let i = 0; i < this.height; i++) {
+      let shape = '';
+      for (let j = 0; j < this.width; j++) {
+        shape += 'X';
+      }
+      console.log(shape);
+    }
+  };
 };
