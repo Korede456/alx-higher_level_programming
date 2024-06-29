7c1067886b812d81e8294669e8aa5f5285d6ea7e
Korede Odeyemi <koredethompson456@gmail.com>
2024-06-28 17:11:15 -0700

    Object relational mapping

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -13,7 +13,8 @@ if __name__ == '__main__':
     database_name = sys.argv[3]
     state_name = sys.argv[4].split('\'')[0]
 
-    db = MySQL.connect(user=username, passwd=password, db=database_name)
+    db = MySQL.connect(host='localhost', user=username, passwd=password,
+                       db=database_name, port=3306)
     cursor = db.cursor()
     qry = """SELECT cities.name FROM cities\
              INNER JOIN states ON cities.state_id = states.id \
