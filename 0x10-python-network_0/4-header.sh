a89ae46d8d8bd5f0cbde00c619146201493de237
Korede Odeyemi <koredethompson456@gmail.com>
2024-07-04 13:14:46 -0700

    Python Network

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,2 +1,3 @@
-# Display all HTTP methods the server of a given URL will accept.
-curl -sI "$1" | grep "Allow" | cut -d " " -f 2-
+#!/bin/bash
+# Send a GET request to a given URL with a header variable.
+curl -sH "X-HolbertonSchool-User-Id: 98" "${1}"
