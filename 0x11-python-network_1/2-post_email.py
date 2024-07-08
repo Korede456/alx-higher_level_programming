8165812e406cab4e9e4dbf89bb98d02aaf6bb143
Korede Odeyemi <koredethompson456@gmail.com>
2024-07-08 03:38:42 -0700

    python Network :

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 import urllib.request
 import urllib.parse
 from sys import argv
@@ -8,7 +7,7 @@ if __name__ == '__main__':
     data = urllib.parse.urlencode({'email': argv[2]})
     data = data.encode('ascii')
     req = urllib.request.Request(argv[1], data)
-
     with urllib.request.urlopen(req) as response:
         query_ = response.read()
-        print(query_.decode('utf-8'))
+
+    print(query_.decode('utf-8'))
