aa1ba44ff3cbe78d5d14d9c83c939b1ce1cdf63b
Korede Odeyemi <koredethompson456@gmail.com>
2024-07-30 02:31:44 -0700

    A script that writes a string to a file

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+
+/* A script that writes a string into a file */
+
+if (process.argv.length < 4) {
+  console.error('Please provide a file path and a string to write as arguments.');
+  process.exit(1);
+}
+
+const filePath = process.argv[2];
+const stringToWrite = process.argv[3];
+
+fs.writeFile(filePath, stringToWrite, 'utf8', (err) => {
+  if (err) {
+    console.error(err);
+  }
+});
