9770fc8238916f18fa31552e4931f3cdb9d758f8
Korede Odeyemi <koredethompson456@gmail.com>
2024-07-30 02:46:38 -0700

    A script that display the status code of a get request

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+
+const request = require('request');
+
+/* A script that prints the status code of a request */
+
+if (process.argv.length < 3) {
+  console.error('Please provide a URL as the first argument.');
+  process.exit(1);
+}
+
+const url = process.argv[2];
+
+request.get(url, (error, response) => {
+  if (error) {
+    console.error(error);
+    return;
+  }
+  console.log(`code: ${response.statusCode}`);
+});
